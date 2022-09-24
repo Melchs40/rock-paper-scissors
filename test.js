496bcfc98ed97fc4369ab288ed737be5b73888e4
Melchs40 <samscodingjourney@gmail.com>
2022-09-24 01:49:26 -0500

    Added function to test.js

---
+++ test.js
@@ -0,0 +1,8 @@
+function  getComputerChoice() {
+    let choice = [
+        "Rock",
+        "Paper",
+        "Scissors"
+    ];
+    let randomChoice = choice[Math.floor(Math.random() * choice.length)];
+    console.log(randomChoice);
