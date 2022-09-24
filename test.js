713f368ab24aa2eaaf53e8599e5181137dd91ccf
Melchs40 <samscodingjourney@gmail.com>
2022-09-24 15:02:46 -0500

    Added playRound function to test.js

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
 function getComputerChoice() {
+
     let choice = [
         "Rock",
         "Paper",
@@ -7,3 +8,17 @@ function getComputerChoice() {
     let randomChoice = choice[Math.floor(Math.random() * choice.length)];
     console.log(randomChoice);
 }
+
+function playRound(playerSelection, computerSelection) {
+
+    if (computerSelection == "Rock") {
+        return("You tied, please try again")
+    } else if (computerSelection == "Scissors") {
+        return("You won! Rock beats scissors!")
+    } else {
+        return("You lost! Paper beats rock!") }
+}
+
+const playerSelection = "Rock".toLowerCase();
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
