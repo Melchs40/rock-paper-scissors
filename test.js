9362cbde0a584de60af47a68a87c16592c000f18
Melchs40 <samscodingjourney@gmail.com>
2022-09-24 16:32:02 -0500

    Changed playRound function in test.js

--- test.js
+++ test.js
@@ -14,10 +14,10 @@ function playRound (playerSelection, computerSelection) {
     let player = playerSelection.toLowerCase();
     let computer = computerSelection.toLowerCase();
     if (player == computer) {
-        return("You tied, please try again")
+        return("You tied, please try again.")
     } else if ((player == "rock" && computer == "scissors") || (player == "paper" && computer == "rock") || (player == "scissors" && computer == "paper")) {
-        return("You won!")
-    } else {return "You lost!"
+        return(`You won!!! ${player.charAt(0).toUpperCase() + player.slice(1)} beats ${computer}!`)
+    } else {return `You lost! ${computer.charAt(0).toUpperCase() + computer.slice(1)} beats ${player}...`
     }
 
 }
