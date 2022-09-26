e03620ed339a4e34b36beefcf64e5377401dd971
Melchs40 <samscodingjourney@gmail.com>
2022-09-26 12:45:44 -0500

    Add game function to test.js

--- test.js
+++ test.js
@@ -9,15 +9,19 @@ function getComputerChoice() {
     return(randomChoice);
 }
 
-function playRound (playerSelection, computerSelection) {
+function playRound (playerSelection, b = computerSelection) {
 
     let player = playerSelection.toLowerCase();
     let computer = computerSelection.toLowerCase();
     if (player == computer) {
-        return("You tied, please try again.")
+        let roundResult = ("You tied, please try again.")
+        return roundResult;
     } else if ((player == "rock" && computer == "scissors") || (player == "paper" && computer == "rock") || (player == "scissors" && computer == "paper")) {
-        return(`You won!!! ${player.charAt(0).toUpperCase() + player.slice(1)} beats ${computer}!`)
-    } else {return `You lost! ${computer.charAt(0).toUpperCase() + computer.slice(1)} beats ${player}...`
+        let roundResult = (`You won!!! ${player.charAt(0).toUpperCase() + player.slice(1)} beats ${computer}!`)
+        return roundResult;
+    } else {
+        let roundResult = `You lost! ${computer.charAt(0).toUpperCase() + computer.slice(1)} beats ${player}...`;
+        return roundResult;
     }
 
 }
@@ -26,3 +30,24 @@ function playRound (playerSelection, computerSelection) {
 const playerSelection = "rock";
 const computerSelection = getComputerChoice();
 console.log(playRound(playerSelection, computerSelection));
+
+function game (playerSelection, b = computerSelection) {
+
+    for (let i = 0; i < 5; i++) {
+
+        playRound(playerSelection, b = computerSelection)
+            if (roundResult.includes("won")) {
+                let playerScore = 0; playerScore < 5, playerScore++;
+                let roundMessage = `You won a round! The score now is ${playerScore} to ${computerScore}`;
+                return roundMessage;
+            } else if (roundResult.includes("lost")) {
+                let computerScore = 0; computerScore < 5, computerScore++;
+                let roundMessage = `You lost a round! The score now is ${playerScore} to ${computerScore}`;
+                return roundMessage;
+            } else {
+                let roundMessage = `You tied, the score is still ${playerScore} to ${computerScore}`;
+                return roundMessage;
+            }
+        }
+
+}
