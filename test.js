9a09cbd5dc1798d9e72d6e447ac0408f8e9a6bb8
Melchs40 <samscodingjourney@gmail.com>
2022-10-08 14:49:08 -0500

    Edit playRound function in test.js

--- test.js
+++ test.js
@@ -40,24 +40,35 @@ function game () {
     for (let i = 0; i < 5; i+0) {
         let playerResponse = prompt("Please choose: Rock, Paper, or Scissors");
         let result = playRound(playerResponse, b = computerSelection);
-        console.log(result);
-        if (result.includes("won")) {
-            i++;
-            if (i === 5) {
-                alert(`The game is over! The final score is ${playerScore} to ${computerScore}`);
-            }
-        } else if (result.includes("lost")) {
-            i++;
-            if (i === 5) {
-                alert(`The game is over! The final score is ${playerScore} to ${computerScore}`);
-            }
-        } else if (result.includes("tied")) {
-            i+0;
-        } else { 
-            console.log(badResult); 
-             }
+        if (playerResponse.toLowerCase() === "rock" || playerResponse.toLowerCase() === "paper" || playerResponse.toLowerCase() === "scissors") {
+            if (result.includes("won")) {
+                console.log(result);
+                i++;
+                if (i === 5) {
+                    alert(`The game is over! You won! The final score is ${playerScore} to ${computerScore}`);
+                    break;
+                } else if (playerScore === 3) {
+                    alert(`The game is over! You won! The final score is ${playerScore} to ${computerScore}`);
+                    break;
+                }
+            } else if (result.includes("lost")) {
+                console.log(result);
+                i++;
+                if (i === 5) {
+                    alert(`The game is over! You lost... The final score is ${playerScore} to ${computerScore}`);
+                    break;
+                } else if (computerScore === 3) {
+                    alert(`The game is over! You lost... The final score is ${playerScore} to ${computerScore}`);
+                    break;
+                }
+            } else
+                console.log(result);
+                i+0;
+        } else 
+            alert(badResult);  
+        }
     }
-}
+
 
 
 game();
