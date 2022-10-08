300ba4d5072a3392c20f1316682a3fff68a675de
Melchs40 <samscodingjourney@gmail.com>
2022-10-08 15:50:38 -0500

    Edit playRound and game functions in test.js

--- test.js
+++ test.js
@@ -13,21 +13,20 @@ let playerScore = 0;
 let computerScore = 0;
 
 
-function playRound (playerSelection, b = computerSelection) {
+function playRound (playerSelection) {
     let player = playerSelection.toLowerCase();
-    let computer = computerSelection.toLowerCase();
-    if (player == computer) {
-        let roundResult = (`You tied, please try again. The score is still ${playerScore} to ${computerScore}`)
+    let computer = getComputerChoice().toLowerCase();
+    if ((computer == "rock" && player == "scissors") || (computer == "paper" && player == "rock") || (computer == "scissors" && player == "paper")) {
+        computerScore++;
+        let roundResult = (`You lost! ${computer.charAt(0).toUpperCase() + computer.slice(1)} beats ${player}... The score is now ${playerScore} to ${computerScore}`);
         return roundResult;
     } else if ((player == "rock" && computer == "scissors") || (player == "paper" && computer == "rock") || (player == "scissors" && computer == "paper")) {
         playerScore++;
-        let roundResult = (`You won!!! ${player.charAt(0).toUpperCase() + player.slice(1)} beats ${computer}! The score now is ${playerScore} to ${computerScore}`)
+        let roundResult = (`You won!!! ${player.charAt(0).toUpperCase() + player.slice(1)} beats ${computer}! The score now is ${playerScore} to ${computerScore}`);
         return roundResult;
-    } else {
-        computerScore++;
-        let roundResult = `You lost! ${computer.charAt(0).toUpperCase() + computer.slice(1)} beats ${player}... The score is now ${playerScore} to ${computerScore}`;
+    } else (player == computer)
+        let roundResult = (`You tied, please try again. The score is still ${playerScore} to ${computerScore}`);
         return roundResult;
-    }
 }
 
 
@@ -39,9 +38,10 @@ let badResult = "Invalid value, Please enter Rock, Paper, or Scissors";
 function game () {
     for (let i = 0; i < 5; i+0) {
         let playerResponse = prompt("Please choose: Rock, Paper, or Scissors");
-        let result = playRound(playerResponse, b = computerSelection);
+        let result = playRound(playerResponse);
         if (playerResponse.toLowerCase() === "rock" || playerResponse.toLowerCase() === "paper" || playerResponse.toLowerCase() === "scissors") {
             if (result.includes("won")) {
+                alert(result);
                 console.log(result);
                 i++;
                 if (i === 5) {
@@ -52,6 +52,7 @@ function game () {
                     break;
                 }
             } else if (result.includes("lost")) {
+                alert(result);
                 console.log(result);
                 i++;
                 if (i === 5) {
@@ -62,6 +63,7 @@ function game () {
                     break;
                 }
             } else
+                alert(result);
                 console.log(result);
                 i+0;
         } else 
