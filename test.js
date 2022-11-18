d20a633f9be0c8d8b32ca37b3474f6742420a8fd
Melchs40 <samscodingjourney@gmail.com>
2022-11-18 15:21:37 -0600

    Edit test.js

--- test.js
+++ test.js
@@ -26,13 +26,23 @@ function playRound () {
     let computer = getComputerChoice().toLowerCase();
     if ((computer == "rock" && player == "scissors") || (computer == "paper" && player == "rock") || (computer == "scissors" && player == "paper")) {
         computerScore++;
-        let roundResult = (`You lost! ${computer.charAt(0).toUpperCase() + computer.slice(1)} beats ${player}... The score is now ${playerScore} to ${computerScore}`);
-        result.textContent = roundResult;
-    } else if ((player == "rock" && computer == "scissors") || (player == "paper" && computer == "rock") || (player == "scissors" && computer == "paper")) {
+        if (computerScore === 5) {
+            let roundResult = (`${computer.charAt(0).toUpperCase() + computer.slice(1)} beats ${player}. You have lost the game... The final score is ${computerScore} to ${playerScore}`);
+            result.textContent = roundResult;
+            buttons.style.display = "none";
+        } else {
+            let roundResult = (`You lost this round! ${computer.charAt(0).toUpperCase() + computer.slice(1)} beats ${player}... The score is now ${playerScore} to ${computerScore}`);
+            result.textContent = roundResult;
+    }} else if ((player == "rock" && computer == "scissors") || (player == "paper" && computer == "rock") || (player == "scissors" && computer == "paper")) {
         playerScore++;
-        let roundResult = (`You won!!! ${player.charAt(0).toUpperCase() + player.slice(1)} beats ${computer}! The score now is ${playerScore} to ${computerScore}`);
-        result.textContent = roundResult;
-    } else {
+        if (playerScore === 5) {
+            let roundResult = (`${player.charAt(0).toUpperCase() + player.slice(1)} beats ${computer}. You have won the game!!! The final score is ${playerScore} to ${computerScore}`);
+            result.textContent = roundResult;
+            buttons.style.display = "none";
+        } else {
+            let roundResult = (`You won!!! ${player.charAt(0).toUpperCase() + player.slice(1)} beats ${computer}! The score now is ${playerScore} to ${computerScore}`);
+            result.textContent = roundResult;
+    }} else {
         let roundResult = (`You tied, please try again. The score is still ${playerScore} to ${computerScore}`);
         result.textContent = roundResult;
     }
