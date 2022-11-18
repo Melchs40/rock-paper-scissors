848c911c46abc3ffed4e7b83ef35cb792034b96a
Melchs40 <samscodingjourney@gmail.com>
2022-11-18 14:57:18 -0600

    Edit index.html and test.js function

--- test.js
+++ test.js
@@ -12,26 +12,28 @@ function getComputerChoice() {
 let playerScore = 0;
 let computerScore = 0;
 
+// New Work
 
-function playRound (playerSelection) {
-    let player = playerSelection.toLowerCase();
+let buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+    button.addEventListener("click", playRound);
+});
+
+let result = document.getElementById("result");
+
+function playRound () {
+    let player = this.id.toLowerCase();
     let computer = getComputerChoice().toLowerCase();
     if ((computer == "rock" && player == "scissors") || (computer == "paper" && player == "rock") || (computer == "scissors" && player == "paper")) {
         computerScore++;
         let roundResult = (`You lost! ${computer.charAt(0).toUpperCase() + computer.slice(1)} beats ${player}... The score is now ${playerScore} to ${computerScore}`);
-        return roundResult;
+        result.textContent = roundResult;
     } else if ((player == "rock" && computer == "scissors") || (player == "paper" && computer == "rock") || (player == "scissors" && computer == "paper")) {
         playerScore++;
         let roundResult = (`You won!!! ${player.charAt(0).toUpperCase() + player.slice(1)} beats ${computer}! The score now is ${playerScore} to ${computerScore}`);
-        return roundResult;
-    } else (player == computer)
+        result.textContent = roundResult;
+    } else {
         let roundResult = (`You tied, please try again. The score is still ${playerScore} to ${computerScore}`);
-        return roundResult;
+        result.textContent = roundResult;
+    }
 }
-
-
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-let badResult = "Invalid value, Please enter Rock, Paper, or Scissors";
-
-
